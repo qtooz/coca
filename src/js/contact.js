3c7ee53889832b7ec159cbac6049b2d19ef56e56
qtooz <qtooz@mail.ru>
2024-05-01 15:54:16 +0700

    create contacts: hero section and partners

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,17 +1,14 @@
-import '../assets/scss/main.scss';
-// components
+import '../assets/scss/contact.scss';
+
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import {
-  useInsightSlider,
-  useTestimonialsSlider,
-} from './components/home/slider.js';
+import { useInsightSlider, useTestimonialsSlider } from './components/home/slider.js';
 import { resizableSwiper } from './components/home/slider-logo.js';
 import { accordionMobile } from './components/home/accardion.js';
+
 useTheme();
 useBurger();
 useInsightSlider();
 resizableSwiper();
 useTestimonialsSlider();
 accordionMobile();
-
