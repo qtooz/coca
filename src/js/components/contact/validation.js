2dc37c0516dcb1f3e603b5b99835915223193c82
qtooz <qtooz@mail.ru>
2024-05-03 16:18:22 +0700

    created contact: get-started section, realized validation form

---
+++ src/js/components/contact/validation.js
@@ -0,0 +1,67 @@
+function validation(form) {
+  function removeError(input) {
+    const parent = input.parentNode;
+    if (parent.classList.contains('error')) {
+      parent.querySelector('.error-label').remove();
+      parent.classList.remove('error');
+    }
+  }
+
+  function createError(input, text) {
+    const parent = input.parentNode;
+    const errorLabel = document.createElement('label');
+
+    errorLabel.classList.add('error-label');
+    errorLabel.textContent = text;
+    parent.classList.add('error');
+
+    parent.append(errorLabel);
+  }
+
+  let result = true;
+
+  const allInputs = form.querySelectorAll('input');
+
+  for (const input of allInputs) {
+		removeError(input);
+
+    if (input.dataset.minLength) {
+      if (input.value.length < input.dataset.minLength) {
+        removeError(input);
+        createError(input, `Минимальное количество символов: ${input.dataset.maxLength}`);
+        result = false;
+      }
+    }
+
+
+    if (input.dataset.maxLength) {
+      if (input.value.length > input.dataset.maxLength) {
+        removeError(input);
+        createError(input, `Максимальное количество символов: ${input.dataset.maxLength}`);
+        result = false;
+      }
+    }
+
+    if (input.dataset.required == 'true') {
+      if (input.value == '') {
+        removeError(input);
+        createError(input, 'Поле не заполнено!');
+        result = false;
+      }
+    }
+  }
+
+  return result;
+}
+
+export const useValidation = () => {
+  document
+    .getElementById('add-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      if (validation(this) == true) {
+        alert('Форма проверена успешно!');
+      }
+    });
+};
--- src/js/contact.js
+++ src/js/contact.js
@@ -2,9 +2,14 @@ import '../assets/scss/contact.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { useInsightSlider, useTestimonialsSlider } from './components/home/slider.js';
+import {
+  useInsightSlider,
+  useTestimonialsSlider,
+} from './components/home/slider.js';
 import { resizableSwiper } from './components/home/slider-logo.js';
 import { accordionMobile } from './components/home/accardion.js';
+import { usePhone } from './components/contact/phone.js';
+import { useValidation } from './components/contact/validation.js';
 
 useTheme();
 useBurger();
@@ -12,3 +17,7 @@ useInsightSlider();
 resizableSwiper();
 useTestimonialsSlider();
 accordionMobile();
+usePhone();
+useValidation();
+
+
