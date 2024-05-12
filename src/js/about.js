57f0981e1efa4e2c2eebc7478379009cc61c8dac
qtooz <qtooz@mail.ru>
2024-05-12 14:57:26 +0700

    create about:hero section

---
+++ src/js/about.js
@@ -0,0 +1,14 @@
+import '/scss/about.scss';
+
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { accordionMobile } from './components/home/accardion.js';
+import { useAboutSlider } from './components/about/heroSwiper.js';
+
+
+useTheme();
+useBurger();
+accordionMobile();
+useAboutSlider();
+
+
---
+++ src/js/components/about/heroSwiper.js
@@ -0,0 +1,47 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, EffectFade, Autoplay } from 'swiper/modules';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
+
+export const useAboutSlider = () => {
+  new Swiper('.about__slider', {
+    slidesPerView: 1.5,
+    spaceBetween: 32,
+    loop: true,
+    centeredSlides: true,
+    breakpoints: {
+      993: {
+        centeredSlides: false,
+      },
+			768: {
+				slidesPerView: 2.5,
+			}
+    },
+  });
+};
+
+export const useTestimonialsSlider = () => {
+  new Swiper('.testimonials__slider', {
+    modules: [Navigation, EffectFade, Autoplay],
+    effect: 'fade',
+
+    fadeEffect: {
+      crossFade: true,
+    },
+
+    slidesPerView: 'auto',
+    spaceBetween: 22,
+    loop: true,
+
+    autoplay: {
+      delay: 1000,
+    },
+    speed: 300,
+
+    navigation: {
+      prevEl: '.testimonials__btn--prev',
+      nextEl: '.testimonials__btn--next',
+    },
+  });
+};
